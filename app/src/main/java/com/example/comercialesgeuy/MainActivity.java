1f5751f2414829bac7541907edd9eb7533c6dabd
ElinaGFL <50997894+ElinaGFL@users.noreply.github.com>
2020-12-10 10:33:32 +0100

    intent para calendario

--- app/src/main/java/com/example/comercialesgeuy/MainActivity.java
+++ app/src/main/java/com/example/comercialesgeuy/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.comercialesgeuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         btnCalendario = findViewById(R.id.button1);
 
         partners= findViewById(R.id.button2);
 
         btnPedidos = findViewById(R.id.button3);
 
+        btnCalendario.setOnClickListener(v -> abrirCalendarioActivity(v));
+    }
+
+    public void abrirCalendarioActivity (View view) {
+        Intent intent = new Intent(this, CalendarioActivity.class);
+        startActivity(intent);
     }
 }
