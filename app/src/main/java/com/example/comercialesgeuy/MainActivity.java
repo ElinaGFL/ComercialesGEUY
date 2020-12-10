a06d26afd9c1cd5651ce5b7f6167226c72beb6b4
jacom15 <Yddlrl15>
2020-12-10 10:14:50 +0100

    Inicializar boton parners y calendario

--- app/src/main/java/com/example/comercialesgeuy/MainActivity.java
+++ app/src/main/java/com/example/comercialesgeuy/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCalendario;
     Button partners;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        partners=(Button)findViewById(R.id.button2);
+
+        btnCalendario = findViewById(R.id.button1);
+
+        partners= findViewById(R.id.button2);
+
     }
 }
