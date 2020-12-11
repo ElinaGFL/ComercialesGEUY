53ec91839b41de3bc5e14cadceb91d1b7339c8b4
unaign <unaign@hotmail.es>
2020-12-11 09:56:35 +0100

    gestionPedido2

--- app/src/main/java/com/example/comercialesgeuy/GestionPedido.java
+++ app/src/main/java/com/example/comercialesgeuy/GestionPedido.java
@@ -10,5 +10,9 @@ public class GestionPedido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion_pedido);
+
+
+
+        opefuideriucedudcuibcjbldf
     }
 }
